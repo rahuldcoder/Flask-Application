6a3426b0859938af6c67309e62b08b4bbe97b256
rahuldcoder <rahulthakur02k@gmail.com>
2018-06-26 09:22:42 -0600

    adding articles

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask,render_template
-
+from data import Articles
 app = Flask(__name__)
+
+
+Articles = Articles()
 
 @app.route('/')
 def index():
@@ -9,6 +12,11 @@ def index():
 @app.route('/about')
 def about():
     return render_template('about.html')    
+
+
+@app.route('/articles')
+def articles():
+    return render_template('articles.html',articles = Articles)    
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ data.py
@@ -0,0 +1,32 @@
+def Articles():
+    articles = [
+
+        {
+            'id': 1,
+            'title': 'Article One',
+            'body': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo dolorum autem vero harum necessitatibus eos laboriosam, molestiae fugit earum? Deserunt cumque dignissimos nemo, recusandae incidunt illum vitae repudiandae voluptates voluptas',
+            'author': 'Rahul Thakur',
+            'create date': '26 June 2018'
+
+        },
+
+
+        {
+            'id': 1,
+            'title': 'Article One',
+            'body': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo dolorum autem vero harum necessitatibus eos laboriosam, molestiae fugit earum? Deserunt cumque dignissimos nemo, recusandae incidunt illum vitae repudiandae voluptates voluptas',
+            'author': 'Rahul Thakur',
+            'create date': '26 June 2018'
+
+        },
+
+
+        {   'id': 1,
+            'title': 'Article One',
+            'body': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo dolorum autem vero harum necessitatibus eos laboriosam, molestiae fugit earum? Deserunt cumque dignissimos nemo, recusandae incidunt illum vitae repudiandae voluptates voluptas',
+            'author': 'Rahul Thakur',
+            'create date': '26 June 2018'
+
+         }
+            ]
+    return articles
